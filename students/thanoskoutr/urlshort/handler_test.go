dbedf1827d5709aae8f0a18fd50699d6f6651b24
thanoskoutr <koutroubas.thanos@gmail.com>
2021-09-28 17:46:17 +0300

    Added testing for YAMLHandler

--- students/thanoskoutr/urlshort/handler_test.go
+++ students/thanoskoutr/urlshort/handler_test.go
@@ -7,11 +7,19 @@ import (
 	"testing"
 )
 
-// Testcases
+// Testcases Maphandler
 var pathsToUrls = map[string]string{
 	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 }
+
+// Testcases YAMLHandler
+var ymls = `
+- path: /urlshort-godoc
+  url: https://godoc.org/github.com/gophercises/urlshort
+- path: /yaml-godoc
+  url: https://godoc.org/gopkg.in/yaml.v2
+`
 
 func TestMapHandler(t *testing.T) {
 	// Run tests for all testcases
@@ -32,16 +40,25 @@ func TestMapHandler(t *testing.T) {
 	}
 }
 
-func TestParseYAML(t *testing.T) {
-	t.Errorf("not implemented")
-}
-
-func TestBuildMap(t *testing.T) {
-	t.Errorf("not implemented")
-}
-
 func TestYAMLHandler(t *testing.T) {
-	t.Errorf("not implemented")
+	// Run tests for all testcases
+	// for i, path := range paths {
+	for path, url := range pathsToUrls {
+		resp := runYAMLHandler(t, []byte(ymls), path)
+
+		// Check the status code is what we expect.
+		if status := resp.StatusCode; status != http.StatusFound {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusFound)
+		}
+
+		// Check the header to see if redirection is what we expect.
+		if header := resp.Header; header["Location"][0] != url {
+			t.Errorf("handler returned wrong url: got %v want %v",
+				header["Location"][0], url)
+		}
+	}
+
 }
 
 // Create a fallback Handler to pass to other Handlers
@@ -66,6 +83,31 @@ func runMapHandler(t *testing.T, pathsToUrls map[string]string, path string) *ht
 	// Call the handler, passing the response and the request
 	mapHandler := MapHandler(pathsToUrls, fallbackHandler)
 	mapHandler(resp, req)
+
+	// Return Response: StatusCode, Header, Body
+	return resp.Result()
+}
+
+// Run the YAMLHandler with the given path
+func runYAMLHandler(t *testing.T, yml []byte, path string) *http.Response {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We create a ResponseRecorder to record the response.
+	resp := httptest.NewRecorder()
+
+	// Create a fallback handler
+	fallbackHandler := http.HandlerFunc(fallback)
+
+	// Call the handler, passing the response and the request
+	yamlHandler, err := YAMLHandler(yml, fallbackHandler)
+	if err != nil {
+		t.Fatal(err)
+	}
+	yamlHandler(resp, req)
 
 	// Return Response: StatusCode, Header, Body
 	return resp.Result()
