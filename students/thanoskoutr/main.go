ce607c07b3cf202812fb8d617304b8f929ac8e8c
thanoskoutr <koutroubas.thanos@gmail.com>
2021-09-28 15:25:02 +0300

    Added YAML flag, file reading, logging

--- students/thanoskoutr/main.go
+++ students/thanoskoutr/main.go
@@ -1,44 +1,65 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/thanoskoutr/urlshort/students/thanoskoutr/urlshort"
 )
 
 func main() {
+	// Parse command-line flag
+	yamlFilename := flag.String("yaml", "urls.yaml", "YAML file with URLs and their short paths")
+	flag.Parse()
+
+	// Create a default request multiplexer as the last fallback
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	mapHandler := createMapHandler(mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yamlHandler := createYAMLHandler(*yamlFilename, mapHandler)
+
+	// Start server
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
 }
 
+// defaultMux is a default request multiplexer for all paths
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	return mux
 }
 
+// hello is a function handler for all paths
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
+}
+
+// createMapHandler creates and returns a Map Hundler
+func createMapHandler(fallback http.Handler) http.HandlerFunc {
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, fallback)
+	return mapHandler
+}
+
+// createYAMLHandler reads the YAML file, creates and returns a YAML Hundler
+func createYAMLHandler(name string, fallback http.Handler) http.HandlerFunc {
+	yamlFile, err := os.ReadFile(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yamlFile), fallback)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return yamlHandler
 }
